1197ff349da117a97591c9c6b92855ffc17683d0
Percy-gif <percymajor@gmail.com>
2020-09-20 14:43:43 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,48 @@
+class Song 
+    
+    @@count = 0
+    @@artists = []
+    @@genres = []
+ 
+
+    attr_accessor :name, :artist, :genre
+
+    def initialize(name, artist, genre)
+
+        @name = name 
+        @artist = artist 
+        @genre = genre 
+
+        @@count += 1 
+        @@artists << self 
+        @@genres << self 
+   
+    end 
+
+    def self.all
+        @@artists
+    end 
+
+    def self.count 
+        @@count
+    end 
+
+    def self.artists
+        @@artists.uniq
+    end 
+
+    def self.genres 
+        @@genres.uniq 
+    end 
+
+ 
+
+    def self.genre_count 
+
+    end 
+
+    def self.artist_count
+        
+    end 
+
+end 
